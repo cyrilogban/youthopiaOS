2e7b9ef121b540c3207ed8d8edbb02c39c3f364d
Cyril Ogban <152878412+cyrilogban@users.noreply.github.com>
2026-06-29 17:15:36 +0100

    Delete export_session.py

--- export_session.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-
-api_id = 30549075
-api_hash = "0d28ccd2a22881769179cf89fbe5696d"
-
-app = Client("susy", api_id=api_id, api_hash=api_hash)
-app.start()
-print("\nYOUR STRING SESSION:\n" + app.export_session_string() + "\n")
-app.stop()
